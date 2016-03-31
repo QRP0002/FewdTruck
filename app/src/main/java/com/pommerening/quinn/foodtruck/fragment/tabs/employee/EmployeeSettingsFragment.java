45a5ef6da3a17bc80e615320b5b69c9e8ff32463
Quinn Pommerening <qpommer0@gmail.com>
2016-03-31 12:08:20 -0400

    Added a broadcast button to the employee settings fragment. They can now set their location in the database.

--- app/src/main/java/com/pommerening/quinn/foodtruck/fragment/tabs/employee/EmployeeSettingsFragment.java
+++ app/src/main/java/com/pommerening/quinn/foodtruck/fragment/tabs/employee/EmployeeSettingsFragment.java
@@ -9,10 +9,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.Toast;
+import android.widget.ToggleButton;
 
 import com.pommerening.quinn.foodtruck.R;
+import com.pommerening.quinn.foodtruck.pojo.GPSLocation;
 import com.pommerening.quinn.foodtruck.pojo.JSONParser;
 
 import org.apache.http.NameValuePair;
@@ -32,8 +35,12 @@ public class EmployeeSettingsFragment extends Fragment {
     private EditText mTruckNameEditText;
     private String mUsername;
     private ProgressDialog pDialog;
+    private ToggleButton mToggleButton;
 
-    private static final String URL = "http://192.168.1.72/webservice/settingemployee.php";
+    private static int broadcastToggle;
+
+    private static final String SETTING_URL = "http://192.168.1.72/webservice/settingemployee.php";
+    private static final String BROADCAST_URL = "http://192.168.1.72/webservice/locationupdate.php";
     private static final String TAG_SUCCESS = "success";
     private static final String TAG_MESSAGE = "message";
 
@@ -54,12 +61,35 @@ public class EmployeeSettingsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_employee_settings, container, false);
+        final GPSLocation gps = GPSLocation.locationSingleton(getActivity());
 
         mNameEditText = (EditText) view.findViewById(R.id.emp_set_name_et);
         mPasswordEditText = (EditText) view.findViewById(R.id.emp_set_password_et);
         mConfirmEditText = (EditText) view.findViewById(R.id.emp_set_confirm_et);
         mEmailEditText = (EditText) view.findViewById(R.id.emp_set_email_et);
         mTruckNameEditText = (EditText)  view.findViewById(R.id.emp_set_truck_et);
+
+        mToggleButton = (ToggleButton) view.findViewById(R.id.emp_set_toggle_button);
+        mToggleButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked) {
+                    if(gps.canGetLocation()) {
+                        broadcastToggle = 1;
+                        final double latitude = gps.getLatitude();
+                        final double longitude = gps.getLongitude();
+                        new Broadcast().execute(mUsername, String.valueOf(broadcastToggle),
+                                String.valueOf(latitude), String.valueOf(longitude));
+                    }
+                } else {
+                    broadcastToggle = 0;
+                    String latitude = "0";
+                    String longitude = "0";
+                    new Broadcast().execute(mUsername, String.valueOf(broadcastToggle),
+                            latitude, longitude);
+                }
+            }
+        });
 
         mSaveButton = (Button) view.findViewById(R.id.emp_set_save_button);
         mSaveButton.setOnClickListener(new View.OnClickListener() {
@@ -119,11 +149,62 @@ public class EmployeeSettingsFragment extends Fragment {
                 params.add(new BasicNameValuePair("truck_name", truckName));
                 JSONParser jParser = new JSONParser();
                 Log.d("Request", "Starting");
-                JSONObject json = jParser.makeHttpRequest(URL, "POST", params);
+                JSONObject json = jParser.makeHttpRequest(SETTING_URL, "POST", params);
 
                 success = json.getInt(TAG_SUCCESS);
                 Log.d("Success: ", json.getString(TAG_SUCCESS));
 
+                if(success == 1) {
+                    return json.getString(TAG_MESSAGE);
+                } else {
+                    return json.getString(TAG_MESSAGE);
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+    }
+
+    private class Broadcast extends AsyncTask<String, Void, String> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected String doInBackground(String... args) {
+            String username = args[0];
+            String broadcast = args[1];
+            String latitude = args[2];
+            String longitude = args[3];
+
+            return changeBroadcast(username, broadcast, latitude, longitude);
+        }
+
+        @Override
+        protected void onPostExecute(String url) {
+            super.onPostExecute(url);
+            if (url != null){
+                Toast.makeText(getActivity(), url, Toast.LENGTH_LONG).show();
+            }
+        }
+
+        private String changeBroadcast(String username, String broadcast, String latitude,
+                                       String longitude) {
+            int success;
+            try {
+                List<NameValuePair> params = new ArrayList<>();
+                params.add(new BasicNameValuePair("username", username));
+                params.add(new BasicNameValuePair("broadcast", broadcast));
+                params.add(new BasicNameValuePair("latitude", latitude));
+                params.add(new BasicNameValuePair("longitude", longitude));
+                JSONParser jParser = new JSONParser();
+                Log.d("Request", "Starting");
+                JSONObject json = jParser.makeHttpRequest(BROADCAST_URL, "POST", params);
+
+                success = json.getInt(TAG_SUCCESS);
+                Log.d("Success: ", json.getString(TAG_SUCCESS));
                 if(success == 1) {
                     return json.getString(TAG_MESSAGE);
                 } else {
