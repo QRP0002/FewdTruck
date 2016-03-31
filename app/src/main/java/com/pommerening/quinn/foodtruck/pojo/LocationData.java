a03de336818cf5cad667e88cf5e44d64b92d464e
Quinn Pommerening <qpommer0@gmail.com>
2016-03-31 15:01:16 -0400

    Added markers for the trucks so that they are able to be seen on the map. Also, added a splash screen to load location data.

---
+++ app/src/main/java/com/pommerening/quinn/foodtruck/pojo/LocationData.java
@@ -0,0 +1,11 @@
+package com.pommerening.quinn.foodtruck.pojo;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * Created by Quinn Pommerening on 3/31/2016.
+ */
+public class LocationData {
+    public static ArrayList<HashMap<String, String>> locationData;
+}
